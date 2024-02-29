739ca612f78156c7c2a7e0dd0c3bac16a3f09d94
RedSpid3r <12657277+RedSpid3r@users.noreply.github.com>
2024-02-29 17:03:39 +0000

    Update dataSource.ts

--- backend/src/data/dataSource.ts
+++ backend/src/data/dataSource.ts
@@ -1,45 +1,48 @@
-import moment from "moment-timezone";
-import path from "path";
-import { DataSource } from "typeorm";
-import { env } from "../env";
-import { backendDir } from "../paths";
+import moment from 'moment-timezone';
+import path from 'path';
+import { DataSource } from 'typeorm';
+import { env } from '../env';
+import { backendDir } from '../paths';
 
-moment.tz.setDefault("UTC");
+moment.tz.setDefault('UTC');
 
-const entities = path.relative(process.cwd(), path.resolve(backendDir, "dist/backend/src/data/entities/*.js"));
-const migrations = path.relative(process.cwd(), path.resolve(backendDir, "dist/backend/src/migrations/*.js"));
+const entities = path.relative(process.cwd(), path.resolve(backendDir, 'dist/backend/src/data/entities/*.js'));
+const migrations = path.relative(process.cwd(), path.resolve(backendDir, 'dist/backend/src/migrations/*.js'));
 
 export const dataSource = new DataSource({
-  type: "mysql",
-  host: env.DB_HOST,
-  port: env.DB_PORT,
-  username: env.DB_USER,
-  password: env.DB_PASSWORD,
-  database: env.DB_DATABASE,
-  charset: "utf8mb4",
-  supportBigNumbers: true,
-  bigNumberStrings: true,
-  dateStrings: true,
-  synchronize: false,
-  connectTimeout: 2000,
+	type: 'mysql',
+	host: env.DB_HOST,
+	port: env.DB_PORT,
+	username: env.DB_USER,
+	password: env.DB_PASSWORD,
+	database: env.DB_DATABASE,
+	charset: 'utf8mb4',
+	supportBigNumbers: true,
+	bigNumberStrings: true,
+	dateStrings: true,
+	synchronize: false,
+	connectTimeout: 2000,
+	ssl: {
+		rejectUnauthorized: true,
+	},
 
-  logging: ["error", "warn"],
+	logging: ['error', 'warn'],
 
-  // Entities
-  entities: [entities],
+	// Entities
+	entities: [entities],
 
-  // Pool options
-  extra: {
-    typeCast(field, next) {
-      if (field.type === "DATETIME") {
-        const val = field.string();
-        return val != null ? moment.utc(val).format("YYYY-MM-DD HH:mm:ss") : null;
-      }
+	// Pool options
+	extra: {
+		typeCast(field, next) {
+			if (field.type === 'DATETIME') {
+				const val = field.string();
+				return val != null ? moment.utc(val).format('YYYY-MM-DD HH:mm:ss') : null;
+			}
 
-      return next();
-    },
-  },
+			return next();
+		},
+	},
 
-  // Migrations
-  migrations: [migrations],
+	// Migrations
+	migrations: [migrations],
 });
